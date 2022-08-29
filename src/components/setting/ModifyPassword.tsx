7b6b02e4e412ef31f299ee7a8fcb4fb305ffbfc4
YeSeul-0w0 <choi333ys@gmail.com>
2022-08-29 22:39:34 +0900

    apply eslint

--- src/components/setting/ModifyPassword.tsx
+++ src/components/setting/ModifyPassword.tsx
@@ -34,8 +34,7 @@ function ModifyPassword() {
         setOnModal(true);
       } catch (err) {
         console.log(err);
-        setOnInfo('비밀번호 변경에 실패' +
-          '했습니다.');
+        setOnInfo('비밀번호 변경에 실패' + '했습니다.');
         setOnModal(true);
       }
     }
